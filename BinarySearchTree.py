72c28689ac20797b21bc0df75efd20e22dc01879
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-04-08 10:15:52 +0530

    Deletion Operation in BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -30,3 +30,30 @@ class BinarySearchTree:
             else:
                 return _search(current.right,target)
         return _search(self.root,target)
+    
+    def delete(self,target):
+        def _delete(current,target):
+            if not current:
+                return None
+            if target < current.data:
+                current.left = _delete(current.left,target)
+            elif target > current.data:
+                current.right = _delete(current.right,target)
+            else :
+                # Used to delete lead node
+                if not current.left and not current.right :
+                    return None
+                # If only one child Node
+                if not current.left:
+                    return current.right
+                if not current.right:
+                    return current.left
+                # when has two childs  
+                successor =  current.right # inorder Successor
+                while successor.left:
+                    successor = successor.left
+                current.data = successor.data
+                current.right = _delete(current.right,successor.data)
+            return current
+        self.root = _delete(self.root,target)
+                
